dc19a44175541f6ed84db994c4c84988cdb1e790
petergardfjall <peter.gardfjall.work@gmail.com>
2019-02-19 20:13:05 +0100

    bump version to 0.1.1

--- immaterial-theme.el
+++ immaterial-theme.el
@@ -6,7 +6,7 @@
 ;; Author: Peter Gardfjäll
 ;; Keywords: themes
 ;; URL: https://github.com/petergardfjall/emacs-immaterial-theme
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "25"))
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
